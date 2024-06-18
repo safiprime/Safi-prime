ca497b1f855cf44f50b85ae7dc83d31b7acb0c08
Moodsobhan <154103518+Moodsobhan@users.noreply.github.com>
2024-06-18 09:32:17 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -1,66 +1,64 @@
+const fs = require('fs');
 const moment = require('moment-timezone');
+const NepaliDate = require('nepali-date');
 
 module.exports = {
   config: {
     name: "info",
-    version: "1.0",
-    aliases: ["inf"],
-    author: "AYANFE ÑÎMÌ",
+    version: "1.6",
+    author: "AceGun",
+    countDown: 5,
     role: 0,
-    cooldown: 5,
     shortDescription: {
       vi: "",
-      en: "Sends information about the bot and admin."
+      en: "Sends information about the bot and admin along with an image."
     },
     longDescription: {
       vi: "",
-      en: "Sends information about the bot and admin."
+      en: "Sends information about the bot and admin along with an image."
     },
-    category: "owner",
+    category: "utility",
     guide: {
-      en: "{pn}"
+      en: "{pn} or {n}"
     },
     envConfig: {}
   },
 
-  onStart: async function ({ message, prefix }) {
-    const botPrefix = prefix; // Use the provided bot prefix
-    const authorName = "KaizenJi";
-    const authorFB = "https://www.facebook.com/known.as.kaizenji";
+  onStart: async function ({ message }) {
+    const botName = "𝙶𝙾𝙺𝚄 𝙱𝙻𝙰𝙲𝙺 😾👀";
+    const botPrefix = ".";
+    const authorName = "𝗦𝗢𝗕𝗛𝗔𝗡 𝗣𝗥𝗜𝗠𝗘";
+    const authorFB = "https://www.facebook.com/moodsobhan";
+    const authorInsta = " NOT FOR SHOW==";
+    const status = "IN RELATIONSHIPS 🥺 ";
+    const imgURLs = [
+      "https://i.imgur.com/mAjLUwm.jpeg",
+    ];
 
-    const now = moment();
+
+    const now = moment().tz('Asia/Dhaka');
     const date = now.format('MMMM Do YYYY');
-    
+    const time = now.format('h:mm:ss A');
+    const nepaliDate = new NepaliDate(now.toDate());
+    const bsDateStr = nepaliDate.format("dddd, DD MMMM");
     const uptime = process.uptime();
     const seconds = Math.floor(uptime % 60);
     const minutes = Math.floor((uptime / 60) % 60);
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+    const uptimeString = `${hours}hrs: ${minutes}min: ${seconds}sec`;
+    const ping = Math.floor(Math.random() * (400 - 20 + 1)) + 20;
+    const selectedImgURL = imgURLs[Math.floor(Math.random() * imgURLs.length)];
 
-    const additionalText = "";
-
-    // Combine the bot information and additional text in a single message
-message.reply(`======[𝗕𝗼𝘁 𝗶𝗻𝗳𝗼:]======
-𝗕𝗼𝘁 𝗽𝗿𝗲𝗳𝗶𝘅: ${botPrefix}
-
-𝗕𝗼𝘁 𝗻𝗮𝗺𝗲 : LuB0T
-
-𝗢𝘄𝗻𝗲𝗿: ${authorName}
- 
-𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: [${authorName}] ${authorFB}
-
- 𝗗𝗮𝘁𝗲: ${date}
-
-𝗨𝗽𝘁𝗶𝗺𝗲: ${uptimeString}
-      
-      ${additionalText}
-    `);
+    message.reply({
+      body: `===「 Bot & Owner Info 」===\n🤖 | Bot Name: ${botName}\n🌐 | Bot Prefix: ${botPrefix}\n🙋‍♂ | AuthorName: ${authorName}\n💙 | FB: ${authorFB}\n🩷 | Insta: ${authorInsta}\n📌 | Status: ${status}\n🗓 | Date: ${date}\n📆 | BsDate:  ${bsDateStr}\n⏰ | Time: ${time}\n✅ | Bot Running: ${uptimeString}\n🛜 | Ping: ${ping}ms\n=====================`,
+      attachment: await global.utils.getStreamFromURL(selectedImgURL)
+    });
   },
 
-  onChat: async function ({ event, message, getLang, prefix }) {
+  onChat: async function({ event, message, getLang }) {
     if (event.body && event.body.toLowerCase() === "info") {
-      this.onStart({ message, prefix });
+      this.onStart({ message });
     }
   }
 };
